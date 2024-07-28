ba6eec6e3d5dc3aedabeb6f78f8a56298deea6a1
Iman Heidari <iman.heydari@gmail.com>
2024-07-28 00:50:45 -0700

    Add privacy and simple landing page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,10 @@
+const Privacy = () => {
+  return (
+    <iframe
+      src="/privacy.html"
+      style={{ width: "100%", height: "100vh", backgroundColor: "white" }}
+    />
+  );
+};
+
+export default Privacy;
